d6891021eca8388d6f83f094116308a34c36ae1b
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-04-27 00:41:54 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.enableCors({
     // true for all origins
     origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: false,
   });
   await app.listen(5000);
 }
