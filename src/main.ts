abe267f803880b5ea4c78540eba885ed671c76bc
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-04-27 15:43:01 +0700

    updated cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // true for all origins
-    origin:  "https://optimity-react-public-side.vercel.app",
+    origin:  ["https://optimity-react-public-side.vercel.app", "http://optimity.htoomaungthait.xyz"],
     // origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
