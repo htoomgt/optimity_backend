7fb7e57cf533b1790786b002c6824674d280d3bc
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-04-27 13:39:23 +0700

    cors with credential true and origin added

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.enableCors({
     // true for all origins
-    // origin: ["http://localhost:5173", "https://optimity-react-public-side.vercel.app"],
-    origin: "*",
+    origin: ["http://localhost:5173", "https://optimity-react-public-side.vercel.app"],
+    // origin: "*",
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: "*",    
     credentials: true,
