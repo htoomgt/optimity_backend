a534726fceb54a62ddf3d2bb2ca7b64b5ae2604e
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-05-06 19:55:49 +0700

    clean app service and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,54 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
 
-  // @Get("services")
-  // getOrgServices(){
-  //   return this.appService.getOrgServices();
-  // }
-
-  // @Get("services/:id")
-  // getOrgServiceById(@Param('id') id:string){
-  //   return this.appService.getOrgServiceById(id);
-  // }
-
-  // @Get("solutions")
-  // getOrgSolutions(){
-  //   return this.appService.getOrgSolutions();
-  // }
-
-  // @Get("solutions/:id")
-  // getOrgSolutionById(@Param('id') id : string){
-  //   return this.appService.getOrgSolutionById(id);
-  // }
-
-  // @Get("about")
-  // getAboutMainContent() {
-  //   return this.appService.getAboutMainContent();
-  // }
-
-  // @Get("visionmission")
-  // getVisionMission() {
-  //   return this.appService.getVisionMission();
-  // }
-
-  // @Get("corevalues")
-  // getCoreValues() {
-  //   return this.appService.getCoreValues();
-  // }
-
-  // @Get('news')
-  // getNews() {
-  //   return this.appService.getNews();
-  // }
-
-  // @Get('vacancy')
-  // getVacancies() {
-  //   return this.appService.getVacancies();
-  // }
 
 }
